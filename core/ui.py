dba17ba484cb4b2350db3a4e8759e05960ed4094
Moiz-205 <moizrajput205@gmail.com>
2026-03-11 01:34:41 +0500

    directory structure updated

---
+++ core/ui.py
@@ -0,0 +1,29 @@
+from utils import animate, cprint
+
+def get_player_input():
+    trigger = input('Pull Trigger (y/n): ')
+    return trigger
+
+def show_outcome(actor, outcome):
+    if actor == 'player':
+        animate('Pulling Trigger')
+
+        if outcome == 'bang':
+            cprint('Bamm! You are dead...', 'red')
+        elif outcome == 'click':
+            cprint('You Survived...!', 'blue')
+
+    elif actor == "machine":
+        animate("Machine Pulling Trigger", delay=0.3)
+
+        if outcome == "bang":
+            cprint("Bamm! Machine's Dead...", "green")
+            cprint("You Survived...", "blue")
+        elif outcome == "click":
+            cprint("Machine Survived...!", "red")
+
+def show_chicken():
+    cprint("You are killed for being a CHICKEN...!", "red")
+
+def show_invalid():
+    cprint("You are killed for being a FOOLISH CHICKEN...!", "red")
