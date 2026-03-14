8564bd4cbade5267b64c20ee585d6f999a0e6d31
Moiz-205 <moizrajput205@gmail.com>
2026-03-15 01:08:35 +0500

    configuration file for settings and constants

---
+++ config.py
@@ -0,0 +1,22 @@
+# constants
+FPS = 30
+WIDTH, HEIGHT = 800, 600
+
+# states
+PLAYER_TURN = 'player_turn'
+MACHINE_TURN = 'machine_turn'
+SPIN = 'spin'
+OUTCOME = 'outcome'
+GAME_OVER = 'game_over'
+
+# color codes
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (180, 0, 0)
+GREEN = (0, 180, 0)
+BLUE = (0, 100, 220)
+YELLOW = (220, 200, 0)
+GRAY = (60, 60, 60)
+
+# animation color codes
+HOVER_RED = (220, 30, 30)
